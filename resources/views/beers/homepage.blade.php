bc42f9662a35ec6ef4e6c59d765a8be5fe4832f2
sabrina-fontana <sabfon@hotmail.it>
2021-03-18 17:11:31 +0100

    installazione bootstrap e creazione tabella

--- resources/views/beers/homepage.blade.php
+++ resources/views/beers/homepage.blade.php
@@ -3,18 +3,43 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
         <title>Laravel</title>
 
     </head>
     <body>
 
-      @php
-        foreach($beers as $beer) {
-
-          echo $beer->nome;
-        }
-      @endphp
+      <table class="table table-hover table-dark">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">nome</th>
+            <th scope="col">birrificio</th>
+            <th scope="col">colore</th>
+            <th scope="col">stile</th>
+            <th scope="col">nazione</th>
+            <th scope="col">gradi</th>
+            <th scope="col">prezzo</th>
+            <th scope="col">immagine</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($beers as $beer)
+            <tr>
+              <td>{{$beer->id}}</td>
+              <td>{{$beer->nome}}</td>
+              <td>{{$beer->birrificio}}</td>
+              <td>{{$beer->colore}}</td>
+              <td>{{$beer->stile}}</td>
+              <td>{{$beer->nazione}}</td>
+              <td>{{$beer->gradi}}</td>
+              <td>{{$beer->prezzo}}</td>
+              <td><img src="{{$beer->immagine}}" height="200px"></td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
 
     </body>
 </html>
--- resources/views/beers/show.blade.php
+++ resources/views/beers/show.blade.php
@@ -1,1 +1,14 @@
-{{$beer}}
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+    <title></title>
+  </head>
+  
+  <body>
+
+  </body>
+</html>
